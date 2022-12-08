2e119e84ddfa4669517f494c2b343131f6250579
bananaeat <qiuyuan463@gmail.com>
2022-12-08 21:24:48 +0000

    Add responsive layout for Connect5

--- src/Connect5/Grid.react.js
+++ src/Connect5/Grid.react.js
@@ -1,11 +1,16 @@
 import { pieceChar } from './Connect5Helper.react'
 
 function Grid(props) {
+    const w = Math.min(parseInt(window.innerWidth / 20), parseInt(window.innerHeight / 12), 40);
     const gridStyle = {
         backgroundColor: (props.gridColor === 0 ? '#FFFFFF' : '#DFDFDF'),
+        width: w + 'px',
+        height: w + 'px',
+        lineheight: w + 'px',
+        fontSize: w/2 + 'px', 
     }
     return (
-        <button className = "square" style={gridStyle} onClick={() => {props.onClick()}}>
+        <button className = "square" style={gridStyle} onClick={() => {console.log(toString(w));props.onClick()}}>
             {pieceChar(props.gridPiece)}
         </button>
     )
