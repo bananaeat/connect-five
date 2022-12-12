6573838573484167a44e16880ce119a5e611a864
bananaeat <qiuyuan463@gmail.com>
2022-12-12 02:21:51 +0000

    Add back comment (dunno why it disappears)

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://cfcusb24hfapfjakk35jxyss7i.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-as7yrd4ytnginhjdyzu3klngr4"
 };
 
 
