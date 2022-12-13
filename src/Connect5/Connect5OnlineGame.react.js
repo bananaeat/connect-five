f7b066dfb8c6bd8696dd22a07c3eec4a8cf88e47
bananaeat <qiuyuan463@gmail.com>
2022-12-13 23:43:35 +0000

    Corrected schema

--- src/Connect5/Connect5OnlineGame.react.js
+++ src/Connect5/Connect5OnlineGame.react.js
@@ -1,5 +1,16 @@
-export function handleCreateOnlineGame(setGameState, setPlayer){
+import { Auth, API } from 'aws-amplify';
+import { createGame } from '../graphql/mutations'
+
+export async function handleCreateOnlineGame(setGameState, setPlayer){
     setGameState(2);
+    const playerID = (await Auth.currentUserInfo()).id;
+    try{
+        const event = await API.graphql({ query: createGame, input: playerID});
+        console.log(event);
+    } catch (error) {
+        console.log("Error: " + JSON.stringify(error));
+    }
+    setPlayer(1);
 }
 
 export function handleJoinOnlineGame(setGameState, setPlayer){
